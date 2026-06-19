722b808fe253932a8b3b69737bdaeb0cd1b913f5
Vitor de Souza <vitor.conceicao090@academico.ufgd.edu.br>
2026-06-18 21:04:39 -0400

    fix: upgrade prisma configuration for v7 on railway

--- proestoque-api/src/prisma/client.ts
+++ proestoque-api/src/prisma/client.ts
@@ -1,9 +1,8 @@
 import 'dotenv/config';
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 import { PrismaClient } from '@prisma/client';
 
-const adapter = new PrismaBetterSqlite3({ url: './dev.db' });
-
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient({
+  datasourceUrl: process.env.DATABASE_URL
+});
 
 export default prisma;
